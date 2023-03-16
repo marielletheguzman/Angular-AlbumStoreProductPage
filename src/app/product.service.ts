1c453c3459403dd575ecba9db6d3c1002ba4c4a6
marielletheguzman <marielledeguzman2020@gmail.com>
2023-03-17 02:30:46 +0800

    3.5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,7 @@ import { Http,Response  } from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
 
 }
