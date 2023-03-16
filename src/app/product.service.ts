60b0054206d7f2615bc92a32d92005db0c471242
marielletheguzman <marielledeguzman2020@gmail.com>
2023-03-17 02:34:16 +0800

    3.8

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,6 +6,7 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
   getAlbum(id:number){
+    this._http.get(this._albumUrl)
   }
   constructor(private _http: Http) { }
 
