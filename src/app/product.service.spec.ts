07f3322dc46620740f437e2d19cebf9f278abd9d
marielletheguzman <marielledeguzman2020@gmail.com>
2023-03-17 11:46:34 +0800

    commit

--- src/app/product.service.spec.ts
+++ src/app/product.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
 
+import { Album } from './album';
+
+
 describe('ProductService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
