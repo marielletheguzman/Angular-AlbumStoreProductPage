b08235cce0b670d9c7b6760e6ffa988c2c7bf7f4
marielletheguzman <marielledeguzman2020@gmail.com>
2023-03-17 12:54:56 +0800

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,12 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+import { RouterModule,Routes} from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/id', component: ProductPageComponent}
+]
 
 @NgModule({
   declarations: [
@@ -23,7 +28,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
